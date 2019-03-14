c9755a01733901ec21710c6206cd6d3854de280a
neyser zana <neyserj.zana@gmail.com>
2019-03-14 03:18:03 -0400

    adding functionality to toolbar buttons

--- src/components/App.js
+++ src/components/App.js
@@ -5,84 +5,115 @@ import Editor from './Editor/Editor';
 class App extends Component {
   // * Our App will be a class component and hanling all state
   state = {
+    // * toolbarButtons will hold the class needed for button icon and also the function the button will invoke when clicked on.
     toolbarButtons: {
       underline: {
         icon: 'fas fa-underline',
-        event: ''
+        event: function() {
+          document.execCommand('underline', false, '');
+        }
       },
       italic: {
         icon: 'fas fa-italic',
-        event: ''
+        event: function() {
+          document.execCommand('italic', false, '');
+        }
       },
       bold: {
         icon: 'fas fa-bold',
-        event: ''
+        event: function() {
+          document.execCommand('bold', false, '');
+        }
       },
       link: {
         icon: 'fas fa-link',
-        event: ''
+        event: function() {
+          const url = prompt('Enter the URL');
+          document.execCommand('createLink', false, url);
+        }
       },
       cut: {
         icon: 'fas fa-cut',
-        event: ''
-      },
-      image: {
-        icon: 'fas fa-file-image',
-        event: ''
+        event: function() {
+          document.execCommand('cut', false, '');
+        }
       },
       undo: {
         icon: 'fas fa-undo',
-        event: ''
+        event: function() {
+          document.execCommand('undo', false, '');
+        }
       },
       redo: {
         icon: 'fas fa-redo',
-        event: ''
+        event: function() {
+          document.execCommand('redo', false, '');
+        }
       },
       color: {
         icon: 'fas fa-tint',
-        event: ''
+        event: function() {
+          const color = prompt('Enter your color in hex ex:#f1f233');
+          document.execCommand('foreColor', false, color);
+        }
       },
       crossText: {
         icon: 'fas fa-strikethrough',
-        event: ''
+        event: function() {
+          document.execCommand('strikeThrough', false, '');
+        }
       },
       delete: {
         icon: 'fas fa-trash-alt',
-        event: ''
+        event: function() {
+          document.execCommand('delete', false, '');
+        }
       },
       selectAll: {
         icon: 'fas fa-mouse-pointer',
-        event: ''
+        event: function() {
+          document.execCommand('selectAll', false, '');
+        }
       },
       copy: {
         icon: 'fas fa-copy',
-        event: ''
+        event: function() {
+          document.execCommand('copy', false, '');
+        }
       },
       alignCenter: {
         icon: 'fas fa-align-center',
-        event: ''
+        event: function() {
+          document.execCommand('justifyCenter', false, '');
+        }
       },
       alignLeft: {
         icon: 'fas fa-align-left',
-        event: ''
+        event: function() {
+          document.execCommand('justifyLeft', false, '');
+        }
       },
       alignRight: {
         icon: 'fas fa-align-right',
-        event: ''
+        event: function() {
+          document.execCommand('justifyRight', false, '');
+        }
       },
       export: {
         icon: 'fas fa-file-export',
         event: ''
       }
     },
+    // * This will hold the all text within the editorBody
     editorBody: ''
   };
 
   // * Will handle all the changes made inside the editor and save it to state for later use.
-  handleEditorChange = (e) => {
+  handleEditorChange = e => {
     const editorBody = e.target.innerHTML;
-    this.setState({editorBody}); 
-  }
+    console.log(editorBody);
+    this.setState({ editorBody });
+  };
 
   render() {
     return (
