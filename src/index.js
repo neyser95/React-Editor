44c37653a24b3c6d09cdcec331b2ddd288d3b66b
neyser zana <neyserj.zana@gmail.com>
2019-03-13 21:27:43 -0400

    setting up folders for components

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {render} from 'react-dom';
+import App from './components/App.js';
+
+render(<App/>, document.getElementById('root'));
+
+
+module.hot.accept(['./components/App.js'], () => {
+  const NextApp = require('./components/App.js').default;
+  render(
+    <NextApp />, document.getElementById('root')
+  );
+});
