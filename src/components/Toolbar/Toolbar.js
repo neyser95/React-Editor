e0badbd2f60a5670025d7b2134378173c6219c43
neyser zana <neyserj.zana@gmail.com>
2019-03-14 03:17:45 -0400

    adding functionality to toolbar buttons

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -3,11 +3,17 @@ import classNames from './Toolbar.css';
 
 // * Made toolBar a functional component and will only handle rendering of the toolbar.
 const toolBar = (props) => {
-  const {toolbarButtons} = props;
+  const { toolbarButtons } = props;
 
+  // * using the toolbarButtons object will create our toolbar buttons with an icon and onClick
   const buttons = Object.keys(props.toolbarButtons).map((button, i) => {
     return (
-      <button key={button + i} className={classNames.Toolbar__btn}>
+      <button
+        key={button + i}
+        className={classNames.Toolbar__btn}
+        onClick={() => {
+          toolbarButtons[button].event();
+        }}>
         <i className={toolbarButtons[button].icon}></i>
       </button>
     );
